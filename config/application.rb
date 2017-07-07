fba3fc4173bd0127d15b5df33a9c093a4e419a39
Nicolas Derecho <nicolas.derecho@gmail.com>
2017-07-07 20:41:57 -0300

    update config

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module DiablolpfApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :delete]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :delete, :head]
       end
     end
 
