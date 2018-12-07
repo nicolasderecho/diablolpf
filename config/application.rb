c70a4f0266547e295fec7a89a6f14c45fcf941d6
Nicolas Derecho <nicolas.derecho@gmail.com>
2018-12-07 20:52:03 -0300

    update fucking cors

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module DiablolpfApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -50,12 +50,12 @@ module DiablolpfApi
       g.orm :mongoid
     end
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :delete, :head]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :delete, :head]
+    #   end
+    # end
 
   end
 end
