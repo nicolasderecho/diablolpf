300ca863b4f8ccf11a8ff5483307d07868171e25
Nicolas Derecho <nicolas.derecho@gmail.com>
2018-12-07 20:47:44 -0300

    update cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.assets.quiet = true
   # Do not dump schema after migrations.
   #config.active_record.dump_schema_after_migration = false
 end
