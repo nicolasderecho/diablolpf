4950a710a31c3de662663fc34281e40fe5c9fa45
Nicolas Derecho <nicolas.derecho@gmail.com>
2018-12-07 21:54:57 -0300

    allow assets (runes and gems)

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -50,12 +50,7 @@ module DiablolpfApi
       g.orm :mongoid
     end
 
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :delete, :head]
-    #   end
-    # end
-
+    config.middleware.use Rack::MethodOverride
+    
   end
 end
